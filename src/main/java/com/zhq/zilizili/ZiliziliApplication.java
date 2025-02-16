28f4f40546245ba23c055f315622c9885cab3b36
zhqlxl <z465719151@outlook.com>
2025-02-16 17:20:14 +0800

    init

---
+++ src/main/java/com/zhq/zilizili/ZiliziliApplication.java
@@ -0,0 +1,13 @@
+package com.zhq.zilizili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiliziliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiliziliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhq/zilizili/ZiliziliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhq.zilizili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiliziliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
